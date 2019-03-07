5b7a18ba8fb83cbc6046688d8af49f444ac2762e
Wassim Khemici <wassimkhemici@Wassims-Air.attlocal.net>
2019-03-06 23:24:35 -0800

    fixed some jvs

--- assets1/javascript/game.js
+++ assets1/javascript/game.js
@@ -8,19 +8,13 @@ $(document).ready(function () {
 
     $("#submit").click(function (event) {
         event.preventDefault();
-        var points = document.getElementById.click("#true");
-        var points2 = document.getElementById.click("#true");
-        var points3 = document.getElementById.click("#true");
+        var points = document.getElementById("#true");
         points++;
-        points2++;
-        points3++;
         console.log("song", $('input[name="song"]:checked').val());
-        alert("Game over! You scored" + (points + points2 + points3) + "points out of 3.");
+        alert("Game over! You scored" + points + "points out of 3.");
 
         })
     
-        $("configreset").click();
-       
 
     //then reset form after the alert 
     //right now, the alert shows if there is 1 win.. but won't count it up to 2 or 3. 
