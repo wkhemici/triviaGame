0faf82cda01387031fbbf85f77e9dcf97fbf09e0
Wassim Khemici <wassimkhemici@calvisitor-10-105-129-143.calvisitor.1918.berkeley.edu>
2019-03-07 10:27:06 -0800

    troubleshooting the timer

--- assets1/javascript/game.js
+++ assets1/javascript/game.js
@@ -1,25 +1,29 @@
 $(document).ready(function () {
 
-    console.log("is loaded!");
-    //this uses jQuery to select the name selected in a radio button and see if it's been checked, the saves its value
-    // $('input[name="genderS"]:checked').val();
+    // var myVar = setInterval(myTimer, 1000);
+    // function myTimer() {
+    //     var d = new Date();
+    //     var t = d.toLocaleTimeString();
+    //     document.getElementById("#time").innerHTML = t;
+
+        var x = setInterval(function() {
+        var seconds = Math.floor((1000 * 60) / 1000);
+        document.getElementById("time").innerHTML = seconds + "s ";
+        x; 
 
 
+        console.log("is loaded!");
+        //this uses jQuery to select the name selected in a radio button and see if it's been checked, the saves its value
+        // $('input[name="genderS"]:checked').val();
 
-    $("#submit").click(function (event) {
-        event.preventDefault();
-        var points = document.getElementById("#true");
-        points++;
-        console.log("song", $('input[name="song"]:checked').val());
-        alert("Game over! You scored" + points + "points out of 3.");
+        $("#submit").click(function (event) {
+            event.preventDefault();
+            var points = document.getElementById("#true");
+            points++;
+            console.log("song", $('input[name="song"]:checked').val());
+            alert("Game over! You scored" + points + "points out of 3.");
+        });
 
-        })
-    
+    });
 
-    //then reset form after the alert 
-    //right now, the alert shows if there is 1 win.. but won't count it up to 2 or 3. 
-  
-   
-
-
-    })
+});
